3f2aa07d6d2c4bbaac317536baf72f7e27c63d02
jrodr <jrodris2005@gmail.com>
2025-05-23 07:48:16 -0600

    parcial 2

--- src/main/java/ed/lab/E01MeetingRooms.java
+++ src/main/java/ed/lab/E01MeetingRooms.java
@@ -1,11 +1,27 @@
 package ed.lab;
 
-import java.util.List;
+import java.util.*;
 
 public class E01MeetingRooms {
 
     public int minMeetingRooms(List<MeetingInterval> meetingIntervals) {
-        return -1;
-    }
+        if (meetingIntervals == null || meetingIntervals.isEmpty()) {
+            return 0;
+        }
 
+
+        meetingIntervals.sort(Comparator.comparingInt(MeetingInterval::startTime));
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (MeetingInterval interval : meetingIntervals) {
+            if (!minHeap.isEmpty() && interval.startTime() >= minHeap.peek()) {
+                minHeap.poll();
+            }
+
+            minHeap.offer(interval.endTime());
+        }
+
+        return minHeap.size();
+    }
 }
--- src/main/java/ed/lab/E02Logger.java
+++ src/main/java/ed/lab/E02Logger.java
@@ -1,12 +1,21 @@
 package ed.lab;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class E02Logger {
+    private final Map<String, Integer> mensajesmap;
 
     public E02Logger() {
+        mensajesmap = new HashMap<>();
     }
 
     public boolean shouldPrintMessage(int timestamp, String message) {
+        if (!mensajesmap.containsKey(message) || timestamp >= mensajesmap.get(message)) {
+            mensajesmap.put(message, timestamp + 10);
+            return true;
+        }
+
         return false;
     }
-
 }
